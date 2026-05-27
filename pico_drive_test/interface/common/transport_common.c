0c9e3bdeea593823c297983f4c4109ecfde9dc06
Animees <m.bogaards@protonmail.com>
2026-05-27 12:11:44 +0200

    Added code for movement, based on Marco's original code in Python but converted to C and without the WIFI capabilities.

---
+++ pico_drive_test/interface/common/transport_common.c
@@ -0,0 +1,14 @@
+#include "transport_common.h"
+
+void usleep(uint64_t us)
+{
+    sleep_us(us);
+}
+
+int clock_gettime(clockid_t unused, struct timespec *tp)
+{
+    uint64_t m = time_us_64();
+    tp->tv_sec = m / 1000000;
+    tp->tv_nsec = (m % 1000000) * 1000;
+    return 0;
+}
